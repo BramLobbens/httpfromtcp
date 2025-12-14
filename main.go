5c7b05ac12dc0b9cc4f3d61fc2bcfe88652f2c2e
Bram Lobbens <bram@bram-debian.lobbens>
2025-12-14 22:31:29 +0100

    additional commit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,15 +14,39 @@ func main() {
 		log.Fatal(err)
 	}
 
+	currentLine := ""
+
 	for {
 		data := make([]byte, 8)
 		count, err := file.Read(data)
+
+		currentData := string(data[:count])
+		currentLine += currentData
+
 		if err == io.EOF {
+			if len(currentLine) > 0 {
+				fmt.Printf("read: %s\n", currentLine)
+			}
 			os.Exit(0)
 		}
+
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("read: %s\n", data[:count])
+
+		if count == 8 {
+			lineParts := strings.Split(currentLine, "\n")
+			partsLength := len(lineParts)
+
+			if partsLength == 1 {
+				continue
+			}
+
+			for _, part := range lineParts[:len(lineParts)-1] {
+				fmt.Printf("read: %s", part)
+			}
+			fmt.Println()
+			currentLine = lineParts[len(lineParts)-1]
+		}
 	}
 }
