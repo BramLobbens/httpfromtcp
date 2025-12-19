61ddf0f0e56e03e33989306b92f891a0e2486673
BramLobbens <lobbensbram@gmail.com>
2025-12-19 11:45:17 +0100

    wip add listener

--- main.go
+++ main.go
@@ -4,64 +4,72 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("messages.txt")
+
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer listener.Close()
 
-receivedLines := getLinesChannel(file)
-	for line := range receivedLines {
-		fmt.Printf("read: %s\n", line)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("CONNECTION ESTABLISHED")
+
+		receivedLines := getLinesChannel(conn)
+		for line := range receivedLines {
+			fmt.Printf("read: %s\n", line)
+		}
 	}
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
+func getLinesChannel(f net.Conn) <-chan string {
 
 	const BYTE_SIZE int = 8
 	ch := make(chan string) //output channel
 	currentLine := ""
 
-go func() {
-	for {
-		data := make([]byte, BYTE_SIZE)
-		count, err := f.Read(data)
+	go func() {
+		for {
+			data := make([]byte, BYTE_SIZE)
+			count, err := f.Read(data)
 
-		currentLine += string(data[:count])
-		
-		if err == io.EOF {
-			if len(currentLine) > 0 {
-				ch <- currentLine
-			}
-			close(ch)
-		}
+			currentLine += string(data[:count])
 
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if count == BYTE_SIZE {
-			lineParts := strings.Split(currentLine, "\n")
-			partsLength := len(lineParts)
-
-			if partsLength == 1 {
-				continue
+			if err == io.EOF {
+				if len(currentLine) > 0 {
+					ch <- currentLine
+				}
+				close(ch)
 			}
 
-			for _, part := range lineParts[:len(lineParts)-1] {
-				ch <- part
+			if err != nil {
+				log.Fatal(err)
 			}
-			
-			currentLine = lineParts[len(lineParts)-1]
+
+			if count == BYTE_SIZE {
+				lineParts := strings.Split(currentLine, "\n")
+				partsLength := len(lineParts)
+
+				if partsLength == 1 {
+					continue
+				}
+
+				for _, part := range lineParts[:len(lineParts)-1] {
+					ch <- part
+				}
+
+				currentLine = lineParts[len(lineParts)-1]
+			}
 		}
-	}
-}()
+	}()
 
 	return ch
-}
-
 }
