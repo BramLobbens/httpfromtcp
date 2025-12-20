99273addf401adb57039eae6dbc8695f0d981c32
BramLobbens <lobbensbram@gmail.com>
2025-12-20 11:16:37 +0100

    simple parse logic

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,8 +1,10 @@
 package request
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"strings"
 )
 
 type Request struct {
@@ -20,13 +22,42 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	if err != nil {
 		log.Fatal("error", "error", err)
 	}
-	foo, err := parseRequestLine((string(r)))
-	if err != nil {
-		log.Fatal("error", "error", err)
+	rl, err := parseRequestLine(r)
+	request := Request{
+		RequestLine: rl,
 	}
-	return nil, nil
+	return &request, err
 }
 
-func parseRequestLine(request string) (string, error) {
-	return "", nil
+func parseRequestLine(request []byte) (RequestLine, error) {
+	// init
+	requestLine := RequestLine{
+		HttpVersion:   "",
+		RequestTarget: "",
+		Method:        "",
+	}
+
+	// split the request on "\r\n"
+	parts := strings.Split(string(request), "\r\n")
+
+	// split the request line into its parts separated by space
+	requestLineParts := strings.Split(parts[0], " ")
+
+	if n := len(requestLineParts); n != 3 {
+		return requestLine, fmt.Errorf(
+			"invalid number of parts in requestline: %d",
+			n,
+		)
+	}
+
+	for _, p := range requestLineParts {
+		if p == "GET" || p == "POST" {
+			requestLine.Method = p
+		} else if strings.IndexByte(p, '/') == 0 {
+			requestLine.RequestTarget = p
+		} else if strings.Contains(p, "HTTP/") {
+			requestLine.HttpVersion = strings.SplitAfter(p, "/")[1]
+		}
+	}
+	return requestLine, nil
 }
