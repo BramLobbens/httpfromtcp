781600d2539b984ddc1cb731d4e5ee10eddfbfe4
BramLobbens <lobbensbram@gmail.com>
2025-12-20 21:37:40 +0100

    wip

--- internal/request/request.go
+++ internal/request/request.go
@@ -10,9 +10,11 @@ import (
 
 type requestState int
 
+const _bufferSize = 8
+
 const (
-	Initialized requestState = iota
-	Done
+	Initialized requestState = iota // 0
+	Done                            // 1
 )
 
 type Request struct {
@@ -28,50 +30,99 @@ type RequestLine struct {
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	req := Request{}
-	lines, err := io.ReadAll(reader)
-	if err != nil {
-		log.Fatal("error", "error", err)
+	req.State = Initialized
+	buffer := make([]byte, _bufferSize)
+	var bufferCopy []byte
+	readToIndex := 0
+	for {
+		if req.State == Done {
+			return &req, nil
+		}
+		i, err := io.ReadAtLeast(reader, buffer, _bufferSize)
+		if err != nil {
+			if err == io.EOF {
+				req.State = Done
+				break
+			}
+			log.Fatal(err)
+		}
+		readToIndex += i // i bytes read from reader
+
+		// to fix logic regarding copying to data out of the buffer
+		// and increasing the slice to be parsed with the copied read data
+		if i == _bufferSize { // buffer fully read
+			bufferCopy = make([]byte, len(buffer)*2)
+			copy(bufferCopy, buffer)
+		}
+		data := bufferCopy[:readToIndex]
+		n, err := req.parse(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n != 0 { // succesfully parsed line
+			copy(bufferCopy, buffer) // Remove the data that was parsed successfully
+			readToIndex -= n         // n bytes parsed in buffer, i - n left
+		}
 	}
-	err = req.parseRequestLine(lines)
-	return &req, err
+	return &req, nil
 }
 
-// func (r *Request) parse(data []byte) (int, error) {
-
-// }
-
-func (req *Request) parseRequestLine(request []byte) error {
-	// split the request on "\r\n"
-	lines := bytes.SplitN(request, []byte("\r\n"), 2)
-	if len(lines) == 0 {
-		return fmt.Errorf("empty request")
+func (r *Request) parse(data []byte) (int, error) {
+	if r.State == Initialized {
+		numOfBytes, err := r.parseRequestLine(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if numOfBytes != 0 {
+			r.State = Done
+		}
+		return numOfBytes, nil
+	} else if r.State == Done {
+		fmt.Errorf("error: trying to read data in a done state")
 	}
+	fmt.Errorf("error: unknown state")
+	return 0, nil
+}
 
+func (req *Request) parseRequestLine(data []byte) (int, error) {
+	// split the request on the first occurrence of "\r\n" if found
+	firstLine, _, found := bytes.Cut(data, []byte("\r\n"))
+	numOfBytes := len(firstLine)
+	// terminal \r\n not yet in chunk return with initialized state
+	if !found {
+		return 0, nil
+	}
 	// split the request line into its parts separated by space
-	parts := strings.Split(string(lines[0]), " ")
-
+	parts := strings.Fields(string(data)) // Fields is equal to Split on " "
 	if n := len(parts); n != 3 {
-		return fmt.Errorf(
+		return numOfBytes, fmt.Errorf(
 			"invalid number of parts in requestline: %d",
 			n,
 		)
 	}
 
 	if err := req.RequestLine.parseParts(parts); err != nil {
-		return err
+		return numOfBytes, err
 	}
-	return nil
+
+	return numOfBytes, nil
 }
 
 func (rl *RequestLine) parseParts(parts []string) error {
+	// Method
 	method := parts[0]
 	if method != strings.ToUpper(method) {
-		return fmt.Errorf("invalid casing in method name")
+		return fmt.Errorf("method token is case-sensitive; standardized methods are defined in all-uppercase")
 	}
 	rl.Method = method
 
+	// Target
+	if strings.IndexRune(parts[1], '/') != 0 {
+		return fmt.Errorf("the client MUST send \"/\" as the path within the origin-form of request-target")
+	}
 	rl.RequestTarget = parts[1]
 
+	// HttpVersion
 	version := strings.SplitAfter(parts[2], "/")[1]
 	if version != "1.1" {
 		return fmt.Errorf("only version 1.1 is supported; provided: '%v'", version)
