4ded0cffa2925fd85018e54413560954116adebe
BramLobbens <lobbensbram@gmail.com>
2025-12-20 10:22:13 +0100

    wip parserequest

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,32 @@
 package request
+
+import (
+	"io"
+	"log"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	r, err := io.ReadAll(reader)
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+	foo, err := parseRequestLine((string(r)))
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+	return nil, nil
+}
+
+func parseRequestLine(request string) (string, error) {
+	return "", nil
+}
