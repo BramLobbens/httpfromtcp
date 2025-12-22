c863bdd1165cb92ca1c8e5e8cd3f6a00b3c359ec
BramLobbens <lobbensbram@gmail.com>
2025-12-22 19:22:14 +0100

    validate field names

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -12,6 +12,24 @@ func NewHeaders() Headers {
 	return make(Headers)
 }
 
+var isTokenChar = [256]bool{
+	'!': true, '#': true, '$': true, '%': true, '&': true,
+	'\'': true, '*': true, '+': true, '-': true, '.': true,
+	'^': true, '_': true, '`': true, '|': true, '~': true,
+}
+
+func init() {
+	for c := byte('a'); c <= 'z'; c++ {
+		isTokenChar[c] = true
+	}
+	for c := byte('A'); c <= 'Z'; c++ {
+		isTokenChar[c] = true
+	}
+	for c := byte('0'); c <= '9'; c++ {
+		isTokenChar[c] = true
+	}
+}
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	crlf := []byte("\r\n")
 
@@ -31,22 +49,34 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
 		parts := bytes.SplitN(line, []byte(":"), 2)
 		if len(parts) != 2 {
-			return 0, false, fmt.Errorf("malformed header: %s", string(line))
+			return numBytesParsed, false, fmt.Errorf("malformed header: %s", string(line))
 		}
 
 		if s := strings.TrimRight(string(parts[0]), " "); len(s) < len(parts[0]) {
-			return 0, false, fmt.Errorf("invalid formatting in field-name")
+			return numBytesParsed, false, fmt.Errorf("invalid formatting in field-name")
 		}
 
-		key := string(bytes.TrimSpace(parts[0]))
+		key := strings.ToLower(string(bytes.TrimSpace(parts[0])))
 		val := string(bytes.TrimSpace(parts[1]))
 
-		if _, ok := h[key]; ok {
-			return 0, false, fmt.Errorf("key-value pair already exists")
+		if !isFieldNameValid(key) {
+			return numBytesParsed, false, fmt.Errorf("invalid token in field-name: %s", key)
 		}
+
+		if _, ok := h[key]; ok {
+			return numBytesParsed, false, fmt.Errorf("key-value pair already exists: %+v", h[key])
+		}
+
 		h[key] = val
 		numBytesParsed += len(line) + len(crlf)
 	}
 
 	return numBytesParsed, false, nil
+}
+
+func isFieldNameValid(fieldName string) bool {
+	isValidToken := func(r rune) bool {
+		return !isTokenChar[r]
+	}
+	return len(fieldName) < 1 || strings.ContainsFunc(fieldName, isValidToken)
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -27,11 +27,11 @@ func TestInvalid(t *testing.T) {
 
 func TestValidSingleHeader(t *testing.T) {
 	headers := NewHeaders()
-	data := []byte("Host: localhost:42069\r\n")
+	data := []byte("Host: localhost:42069\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 }
@@ -39,6 +39,28 @@ func TestValidSingleHeader(t *testing.T) {
 func TestInvalidSpacing(t *testing.T) {
 	headers := NewHeaders()
 	data := []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
+
+func TestValidTwoHeaders(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nUser-Agent: curl/8.14.0\r\nAccept: */*\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "curl/8.14.0", headers["user-agent"])
+	assert.Equal(t, "*/*", headers["accept"])
+	assert.Equal(t, 61, n)
+	assert.False(t, done)
+}
+
+func TestInvalidToken(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
